4d2b3a4ca87d17193fbdd22eb88d455efd7e6369
Marcelo <marcelovvrodrigues@gmail.com>
2021-07-18 20:40:51 -0300

    Adicionar arquivos de projeto.

---
+++ ApiViaCep_Refit/Program.cs
@@ -0,0 +1,31 @@
+﻿using ApiViaCep_Refit.Repository;
+using Refit;
+using System;
+using System.Threading.Tasks;
+
+namespace ApiViaCep_Refit
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var cepClient = RestService.For<ICepApiService>("http://viacep.com.br");
+                Console.WriteLine("Informe seu cep");
+                var cep = Console.ReadLine().ToString();
+                Console.WriteLine($"Consultando informações do CEP: {cep}");
+
+                var address = await cepClient.GetCep(cep);
+
+                Console.WriteLine($"\nLogradouro:{address.Logradouro}.\nBairro:{address.Bairro}.\nCidade:{address.Localidade}");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro na consulta cep. " + e.Message);
+            }
+
+        }
+    }
+}
---
+++ ApiViaCep_Refit/Repository/ICepApiService.cs
@@ -0,0 +1,16 @@
+﻿using ApiViaCep_Refit.Model;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiViaCep_Refit.Repository
+{
+    public interface ICepApiService
+    {
+        [Get("/ws/{cep}/json")]
+        Task<CepResponse> GetCep(string cep);
+    }
+}
